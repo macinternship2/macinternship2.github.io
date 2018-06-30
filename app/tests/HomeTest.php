bad240b7d07ac46626c7452b306f25e8046804e7
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-06-30 16:06:42 -0400

    fix cs

--- app/tests/HomeTest.php
+++ app/tests/HomeTest.php
@@ -4,8 +4,8 @@ class HomeTest extends TestCase
 {
     public function testGet()
     {
-		$content = $this->get('/')->seeStatusCode(200)->response->getContent();
-		$this->assertTrue(strpos($content, 'Sign in / Sign up') !== false);
-		$this->assertTrue(strpos($content, 'Search by keyword(s)') !== false);
+        $content = $this->get('/')->seeStatusCode(200)->response->getContent();
+        $this->assertTrue(strpos($content, 'Sign in / Sign up') !== false);
+        $this->assertTrue(strpos($content, 'Search by keyword(s)') !== false);
     }
 }
