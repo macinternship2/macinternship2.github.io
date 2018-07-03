2dd55eba8bd8171817e6220d6063472d528336bd
Henry-Hao <haoyixian1994@hotmail.com>
2018-07-03 17:43:45 -0400

    app: started making UI for submitting suggested edits

--- app/resources/views/pages/location_report/collapsed.blade.php
+++ app/resources/views/pages/location_report/collapsed.blade.php
@@ -6,6 +6,15 @@
 	<script language="JavaScript" src="/js/jquery-3.1.1.js"></script>	
 	<script src="/js/location_report.js"></script>
 	<script language="JavaScript" src="/js/pie_graph.js"></script>
+	<script src="/js/bootstrap.js"></script>
+	<style>
+		.fade{
+			opacity:1;
+		}
+		.modal-dialog{
+			top:100px;
+		}
+	</style>
 @stop
 @section('footer-content')
 	@if (!$turn_off_maps)
@@ -27,6 +36,10 @@
 		</div>
 		<div class="location-tags text-right">
 			<div class="location-tags">
+			<a class="location-tag" title="Update Information" href="#" data-toggle="modal" data-target="#suggestionModal">
+				<span class="name">Update Information</span>
+				<span class="icon fa fa-bug"></span>
+			</a>
 			@foreach ( $location->tags()->orderBy('name')->get() as $location_tag )
 				<a class="location-tag" title="{{ $location_tag->name }}" href="/location-search?location_tag_id={{ $location_tag->id }}">
 					<span class="name">{{ $location_tag->name }}</span>
@@ -110,6 +123,49 @@
 		</div>
 		<div id="map">
 		</div>
+		<div class="modal fade" id="suggestionModal" tabindex="-1" role="dialog" 
+			aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" 
+							aria-hidden="true">x
+						</button>
+						<h4 class="modal-title" id="myModalLabel">
+							Update Information
+					</h4>
+					</div>
+					<div class="modal-body">
+						<form class="form" method="POST">
+							<div class="input-group">
+								<label for="location-name">Location name:</label>
+								<input type="text" class="form-control"  name="location-name" id="location-name" value="{{ $location->name }}">
+							</div>
+							<div class="input-group">
+								<label for="phone-number">Phone number:</label>
+								<input type="text" class="form-control" name="phone-number" id="phone-number" value="{{ $location->phone_number }}">
+							</div>
+							<div class="input-group">
+								<label for="address">Address:</label>
+								<input type="text" class="form-control" name="address" id="address" value="{{ $location->address }}">
+							</div>
+							<div class="input-group">
+								<label for="website">External website:</label>
+								<input type="text" class="form-control" name="website" id="website" value="{{ $location->external_web_url }}">
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+					<button type="button" class="btn btn-default" 
+						data-dismiss="modal">Close
+					</button>
+					<button type="button" class="btn btn-primary" id='modifyParentConfirm' data-dismiss="modal">
+						Confirm
+					</button>
+					</div>
+				</div><!-- /.modal-content -->
+			</div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
 	</div>
 </div>
 
