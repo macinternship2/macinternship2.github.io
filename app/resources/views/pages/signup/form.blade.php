4ace182c8be4dda51a2cc197962092c1cd689e83
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-07-05 17:55:23 -0400

    app: update the signin and signup views

--- app/resources/views/pages/signup/form.blade.php
+++ app/resources/views/pages/signup/form.blade.php
@@ -9,27 +9,49 @@
 	<div class="row">
 		<div class="col-md-6 col-xs-12">
 			<div class="signup-form">
+				@if (Session::has('confirm_message'))
+					<div class="alert alert-success">
+						<p>{{Session::get('confirm_message')}}</p>
+					</div>
+				@endif
 				<form method="post" action="/signup">
 					{!! csrf_field() !!}
-					@include('pages.validation_messages', array('errors'=>$errors))
 					<div class="row">
 						<div class="col-xs-12">
 							<input class="clean" name="email" placeholder="Email" value="{{ old('email') }}">
+							@if($errors->has('email'))
+								<p class="text-danger">{{$errors->get('email')[0]}}</p>
+							@endif
 						</div>
 						<div class="col-xs-6">
 							<input class="clean" name="last_name" placeholder="Last Name" value="{{ old('last_name') }}">
+							@if($errors->has('last_name'))
+								<p class="text-danger">{{$errors->get('last_name')[0]}}</p>
+							@endif
 						</div>
 						<div class="col-xs-6">
 							<input class="clean" name="first_name" placeholder="First Name" value="{{ old('first_name') }}">
+							@if($errors->has('first_name'))
+								<p class="text-danger">{{$errors->get('first_name')[0]}}</p>
+							@endif
 						</div>
 						<div class="col-xs-12">
 							<input class="clean" name="password" type="password" placeholder="Password" value="{{ old('password') }}">
+							@if($errors->has('password'))
+								<p class="text-danger">{{$errors->get('password')[0]}}</p>
+							@endif
 						</div>
 						<div class="col-xs-12">
 							<input class="clean" name="password_confirm" type="password" placeholder="Type your password again" value="{{ old('password_confirm') }}">
+							@if($errors->has('password_confirm'))
+								<p class="text-danger">{{$errors->get('password_confirm')[0]}}</p>
+							@endif
 						</div>
 						<div class="col-xs-12">
 							{!! app('captcha')->display(); !!}
+							@if($errors->has('g-recaptcha-response'))
+								<p class="text-danger">{{$errors->get('g-recaptcha-response')[0]}}</p>
+							@endif
 						</div>
 					</div>
 					<div>
@@ -42,11 +64,11 @@
 			<div class="social-media-signins">
 				Or sign in using your social media account
 				
-				<a rel="nofollow" class="facebook" href="/socialauth/auth/Facebook">
+				<a rel="nofollow" class="facebook" href="/social_auth/callback/facebook">
 					<i class="fa-lg fa fa-facebook"></i>
 					<div class="pull-right">Sign in with facebook</div>
 				</a>
-				<a rel="nofollow" class="google-plus" href="/socialauth/auth/Google">
+				<a rel="nofollow" class="google-plus" href="/social_auth/callback/google">
 					<i class="fa-lg fa fa-google-plus"></i>
 					<div class="pull-right">Sign in with Google</div>
 				</a>
