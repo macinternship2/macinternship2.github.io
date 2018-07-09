4958bff435228a999830693b21a73a6dc343ee6c
codemaster94sb <laveen1994vasnani@outlook.com>
2018-07-08 21:10:20 -0400

    Removed the dialog box that pops up in Chrome when changing page from Profile -> Contact

--- app/public/js/profile_save_prompt.js
+++ app/public/js/profile_save_prompt.js
@@ -1,12 +1,9 @@
 $(window).on('beforeunload', function(e) {
 	if ( submitted ) {
 		return;
-	}
+	}	
 	if ( e === undefined ) {
 		e = window.event;
-	}
-	if ( isProfileChanged() ) {
-		return '';
 	}
 	return undefined;
 });
