6480470ba17572e10a051a6733a0c001dbe4be3b
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-07-12 17:29:07 -0400

    app: removed unused files during location report.

--- app/resources/views/pages/location_report/personal_not_available.blade.php
+++
@@ -1,4 +0,0 @@
-				<div class="text-center">
-					<p>The personal accessibility ratings are available only after you
-						have <a href="/signin?message=Sign+in+to+see+personal+ratings&amp;after_signin_redirect=%2Flocation-report%2F{{ $location_id }}%2Fpersonal">signed in</a> and have specified your accessibility needs.</p>
-				</div>
