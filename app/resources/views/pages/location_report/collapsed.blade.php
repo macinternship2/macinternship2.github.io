13c72959f23212925a6d199e32e7e7b343fc7d4c
parth7676 <parth7676@gmail.com>
2018-07-26 21:54:11 -0400

    Review Changes for issue #625.

--- app/resources/views/pages/location_report/collapsed.blade.php
+++ app/resources/views/pages/location_report/collapsed.blade.php
@@ -22,9 +22,10 @@
 		'location' => $location))
 	<div class="basic-info">
 		<div class="addresses">
-		<p><em class="fa fa-map-marker"></em>{{ $location->address }}</p>
-		@if(!is_null($location->phone_number))<p><em class="fa fa-phone"></em>{{ $location->phone_number }}</p>@endif
+		<address><em class="fa fa-map-marker"></em>{{ $location->address }}
+		@if(!is_null($location->phone_number))<p><em class="fa fa-phone"></em><a href="tel: {{ $location->phone_number }}">{{ $location->phone_number }}</a></p>@endif
 		<a href="{{ $location->getExternalWebURL() }}">{{ $location->getExternalWebURL() }}</a>
+		</address>
 		</div>
 		<div class="location-tags text-right">
 			<div class="location-tags">
